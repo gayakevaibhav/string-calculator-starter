2c52d149f8b492c72c302b9e95a5ccb997b3e6bb
Vaibhav Gayake <gayakevaibhav@gmail.com>
2021-10-16 17:29:00 +0530

    Method modified to throw an exception for a negative number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,31 +3,41 @@ package calculator;
 class StringCalculator {
 
 	private final String delimiter = ",|\n|;|/";
-	public int add(String input) {
+
+	public int add(String input) throws Exception {
 
 		String[] numbers = input.split(delimiter);
 
 		if (isEmpty(input)) {
 			return 0;
-		} 
-		if(input.length()==1) {
-			return stringToInt(input);
 		}
-		else {
+		if (input.length() == 1) {
+			return stringToInt(input);
+		} else {
 			return getSum(numbers);
 		}
 
 	}
-	   private int getSum(String numbers[])  {    	
-	      	int sum=0;
-	    	for(String current:numbers) {
-	    		if(stringToInt(current)>1000) {
-	    			continue;
-	    		}
-	    		 sum = sum+Integer.parseInt(current);
-	    	}
-	    	return sum;
-	    }
+
+	private int getSum(String numbers[]) throws Exception {
+		findDangerousInput(numbers);
+		int sum = 0;
+		for (String current : numbers) {
+			if (stringToInt(current) > 1000) {
+				continue;
+			}
+			sum = sum + Integer.parseInt(current);
+		}
+		return sum;
+	}
+
+	private void findDangerousInput(String numbers[]) throws Exception {
+		for (String current : numbers) {
+			if (stringToInt(current) < 0) {
+				throw new Exception("negatives not allowed");
+			}
+		}
+	}
 
 	private boolean isEmpty(String input) {
 		return input.isEmpty();
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -16,13 +16,13 @@ class StringCalculatorShould {
 	}
 
 	// Below method takes empty String and returns 0
-	public void empty_string_should_return_0() {
+	public void empty_string_should_return_0() throws Exception {
 		assertEquals(0, stringCalculator.add(""));
 	}
 
 	// Below method takes single number in String format and returns a number as
 	// integer
-	public void string_with_single_number_should_return_number_as_int() {
+	public void string_with_single_number_should_return_number_as_int() throws Exception {
 		assertEquals(stringCalculator.add("1"), 1);
 	}
 
@@ -40,11 +40,15 @@ class StringCalculatorShould {
 	public void twoNumbersNewLineDelimitedReturnSum() throws Exception {
 		assertEquals(stringCalculator.add("1\n2,3"), 6);
 	}
-	
-	//below method to test weather our method supports different delimiter or not
-	 public void multipleDelimiterReturnSum() throws Exception {
-	    	assertEquals(stringCalculator.add("1;2"),3);
-	    }
-	    
-	
+
+	// below method to test weather our method supports different delimiter or not
+	public void multipleDelimiterReturnSum() throws Exception {
+		assertEquals(stringCalculator.add("1;2"), 3);
+	}
+
+	// Below Method checks the exception in case of negative number
+	@Test(expectedExceptions = Exception.class)
+	public void negativeInputReturnsException() throws Exception {
+		stringCalculator.add("-1");
+	}
 }
