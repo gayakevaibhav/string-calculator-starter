6b4f78f6b6eedd57edb5953018eb2586cfe46a4e
Vaibhav Gayake <gayakevaibhav@gmail.com>
2021-10-16 16:49:08 +0530

    added a test case for add which accepts a single number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,18 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+       if(isEmpty(input)) {
+    	   return 0;
+       }
+	return stringToInt(input);
+    }
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    
+    private int stringToInt(String input) {
+    	return Integer.parseInt(input);
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -2,16 +2,29 @@ package calculator;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
+@Test
 class StringCalculatorShould {
+	
+	private StringCalculator stringCalculator;
+	
+	@BeforeTest
+	public void init() {
+		stringCalculator = new StringCalculator();
+	}
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
+	//Below method takes empty String and returns 0 
+    public void empty_string_should_return_0() {
         assertEquals(0, stringCalculator.add(""));
     }
-
- 
+    
+    //Below method takes single number in String format and returns a number as integer 
+    public void string_with_single_number_should_return_number_as_int() {
+        assertEquals(stringCalculator.add("1"),1);
+    }
+    
+    
+    
 }
