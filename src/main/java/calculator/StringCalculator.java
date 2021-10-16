62f3c3f4c12f16f64843ad5edf410ee7f343477e
Vaibhav Gayake <gayakevaibhav@gmail.com>
2021-10-16 17:41:11 +0530

    Methods modified successfully

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,10 @@
 package calculator;
 
 class StringCalculator {
-
+	//delimiter
 	private final String delimiter = ",|\n|;|/";
-
+	
+	//method which is getting called 
 	public int add(String input) throws Exception {
 
 		String[] numbers = input.split(delimiter);
@@ -18,7 +19,8 @@ class StringCalculator {
 		}
 
 	}
-
+	
+	//this method add all numbers which are passed in string format
 	private int getSum(String numbers[]) throws Exception {
 		findDangerousInput(numbers);
 		int sum = 0;
@@ -30,7 +32,8 @@ class StringCalculator {
 		}
 		return sum;
 	}
-
+	
+	//this method throws exception for negative numbers
 	private void findDangerousInput(String numbers[]) throws Exception {
 		for (String current : numbers) {
 			if (stringToInt(current) < 0) {
@@ -38,11 +41,13 @@ class StringCalculator {
 			}
 		}
 	}
-
+	
+	//this method check weather the string is empty or not
 	private boolean isEmpty(String input) {
 		return input.isEmpty();
 	}
 
+	//this method is to change string into integer format
 	private int stringToInt(String input) {
 		return Integer.parseInt(input);
 	}
